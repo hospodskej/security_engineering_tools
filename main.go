37a356dba623067dca5ecc1a974db32c7e9bff1c
ronin <ronin@osaka.osaka.lab>
2026-05-31 17:28:55 +0200

    QoL changes

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"net"
 	"sort"
+	"time"
+
+	"github.com/pterm/pterm"
 )
 
 func worker(ports, results chan int) {
 	for p := range ports {
+		fmt.Printf("Checked port: %d\n", p) //QA
 		address := fmt.Sprintf("scanme.nmap.org:%d", p)
-		conn, err := net.Dial("tcp", address)
+		conn, err := net.DialTimeout("tcp", address, 1*time.Second)
 		if err != nil {
 			results <- 0
 			continue
@@ -20,7 +24,18 @@ func worker(ports, results chan int) {
 }
 
 func main() {
-	ports := make(chan int, 100)
+	var workers int
+	for {
+		pterm.FgCyan.Printf("How many workers would you like to use?\n")
+		_, err := fmt.Scanln(&workers)
+		if err == nil {
+			break
+		} else {
+			pterm.FgYellow.Printf("Please input a valid number")
+		}
+	}
+
+	ports := make(chan int, workers)
 	results := make(chan int)
 	var openports []int
 
@@ -43,8 +58,14 @@ func main() {
 
 	close(ports)
 	close(results)
-	sort.Ints(openports)
-	for _, port := range openports {
-		fmt.Printf("%d open\n", port)
+	fmt.Println("--------------------------------")
+
+	if len(openports) == 0 {
+		fmt.Println("No open ports available")
+	} else {
+		sort.Ints(openports)
+		for _, port := range openports {
+			fmt.Printf("%d open\n", port)
+		}
 	}
 }
