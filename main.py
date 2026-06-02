989ad5ac7dd448c6433bdc4763dde490002004fc
ronin <ronin@osaka.osaka.lab>
2026-06-02 16:14:12 +0200

    Updated to Python3, debugging

--- main.py
+++ main.py
@@ -1,179 +1,127 @@
+import argparse
 import socket
-import sys
-import getopt
-import threading
+import shlex
 import subprocess
+import sys
+import textwrap
+import threading
 
-listen = False
-command = False
-upload = False
-execute = ""
-target = ""
-upload_destination = ""
-port = 0
+def execute(cmd):
+	cmd = cmd.strip()
+	if not cmd:
+		return
+	output = subprocess.check_output(shlex.split(cmd),stderr=subprocess.STDOUT)
+	return output.decode()
 
-def usage():
-	print "Cuckoo"
-	print
-	print "Usage: cuckoo -t <target_host> -p <port>"
-	print "-l --listen			- listen on [host]:[port] for incoming connections"
-	print "-e --execute=<file_to_run>	- execute the given file upon receiving a connection"
-	print "-c --command			- initialize a command shell"
-	print "-u --upload=<destination>	- upon receiving connection upload a file and write to <destination>"
-	print
-	print
-	print "Examples: "
-	print "cuckoo -t 192.168.0.1 -p 5555 -l -c"
-	print "cuckoo -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe"
-	print "cuckoo -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\""
-	print "echo 'Hello There" | ./cuckoo -t 192.168.11.12 -p 135"
-	sys.exit(0)
-
-def main():
-	global listen
-	global port
-	global execute
-	global command
-	global upload_destination
-	global target
-
-	if not len(sys.argv[1:]):
-		usage()
-
-try:
-	opts, args = getopt.getopt(sys.argv[1:], "hle:t:p:cu:", ["help", "listen", "execute", "target", "port", "command", "upload"])
-except getopt.GetoptError as err:
-	print str(err)
-	usage()
-
-
-for o,a in opts:
-	if o in ("-h", "--help"):
-		usage()
-	elif o in ("-l", "--listen"):
-		listen = True
-	elif o in ("-e", "--execute"):
-		execute = a
-	elif o in ("-c", "--commandshell""):
-		command = True
-	elif o in ("-u", "--upload"):
-		upload_destination = a
-	elif o in ("-t" "--target"):
-		target = a
-	elif o in ("-p", "--port"):
-		port = int(a)
-	else:
-		assert False, "Unhandled Option"
-
-if not listen and len(target) and port > 0:
-
-	# CTRL-D to bypass
-	buffer = sys.stdin.read()
-	client_sender(buffer)
-
-if listen:
-	server_loop()
-
-main()
-
-def client_sender(buffer):
-	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	try:
-		client.connect((target,port))
-		if len(buffer):
-			client.send(buffer)
-
-		while True:
-			recv_len = 1
-			response = ""
-
-			while recv_len:
-				data = client.recv(4096)
-				recv_len = len(data
-				reponse+= data
-
-				if recv_len < 4096:
-					break
-
-			print response,
-
-			buffer = raw_input("")
-			buffer += "\n"
-
-			client.send(buffer)
-
-	except:
-		pring "[*] Exception! Exiting."
-		client.close()
-
-def server_loop():
-	global target
-
-	# no target = listen everywhere
-	if not len(target):
-		target = "0.0.0.0"
-
-	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server.bind((target,port))
-	server.listen(5)
-
-	while True:
-		client_socket, addr = server.accept()
-
-		client_thread = threading.Thread(target=client_handler, args =(client_socket,))
-		client_thread.start()
-
-def run_command(command):
-	command = command.rstrip()
-
-	try:
-		output = subprocess.check_output(command,stderr=subprocess.STDOUT, shell=True)
-	except:
-		output = "Failed to execute command. \r\n"
-
-	return output
-
-def client_handler(client_socket):
-	global upload
-	global execute
-	global command
-
-	# check for upload
-	if len(upload_destination):
-
-		file_buffer =""
-
-		while True:
-			data = client_socket.recv(1024)
-
-			if not data:
-				break
-			else:
-				file_buffer += data
+class NetCat:
+	def __init__(self, args, buffer=None):
+		self.args = args
+		self.buffer = buffer
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	def run(self):
+		if self.args.listen:
+			self.listen()
+		else:
+			self.send()
+	def send(self):
+		self.socket.connect((self.args.target, self.args.port))
+		if self.buffer:
+			self.socket.send(self.buffer)
 
 		try:
-			file_descriptor = open(upload_destination, "wb")
-			file_descriptor.write(file_buffer)
-			file_descriptor.close()
+			while True:
+				recv_len = 1
+				response = ''
+				while recv_len:
+					data = self.socket.recv(4096)
+					recv_len = len(data)
+					response += data.decode()
+					if recv_len < 4096:
+						break
+					if response:
+						print(response)
+						buffer = input('> ')
+						buffer += '\n'
+						self.socket.send(buffer.encode())
+		except KeyboardInterrupt:
+			print('User terminated.')
+			self.socket.close()
+			sys.exit()
 
-			client_socket.send("Successfully saved file to %s\r\n" % upload_destination)
-
-		except:
-			client_socket.send("Failed to save file to %s\r\n" % upload_destination)
-
-	# check for command execution
-	if len(execute):
-		output = run_command(execute)
-		client_socket.send(output)
-
-	# check for command shell
-	if command:
+	def listen(self):
+		self.socket.bind((self.args.target, self.args.port))
+		self.socket.listen(5)
 		while True:
-			client_socket.send("<CCK:#> ")
-			cmd_buffer = ""
-			while "\" not in cmd_buffer:
-				cmd_buffer += client_socket.recv(1024)
+			client_socket, _ = self.socket.accept()
+			client_thread = threading.Thread(
+				target = self.handle, args = (client_socket,)
+			)
+			client_thread.start()
 
-			response = run_command(cmd_buffer)
-			client_socket.send(response)
+	def handle(self, client_socket):
+		if self.args.execute:
+			output = execute(self.args.execute)
+			client_socket.send(output.encode())
+
+		elif self.args.upload:
+			file_buffer = b''
+			while True:
+				data = client_socket.recv(4096)
+				if data:
+					file_buffer += data
+				else:
+					break
+
+			with open(self.args.upload, 'wb') as f:
+				f.write(file_buffer)
+			message = f'Saved file {self.args.upload}'
+			client_socket.send(message.encode())
+
+		elif self.args.command:
+			cmd_buffer = b''
+			while True:
+				try:
+					client_socket.send(b'CCK: #> ')
+					while '\n' not in cmd_buffer.decode():
+						cmd_buffer += client_socket.recv(64)
+					response = execute(cmd_buffer.decode())
+					if response:
+						client_socket.send(response.encode())
+					cmd_buffer = b''
+				except Exception as e:
+					print(f'server killed {e}')
+					self.socket.close()
+					sys.exit()
+def main():
+	parser = argparse.ArgumentParser(
+		description = 'Cuckoo',
+		formatter_class=argparse.RawDescriptionHelpFormatter,
+		epilog=textwrap.dedent('''Example:
+			cuckoo -t 192.168.1.108 -p 5555 -l -c
+			cuckoo -t 192.168.1.108 -p 5555 -l -u=example.txt
+			cuckoo -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\"
+			echo 'TEST' | ./cuckoo -t 192.168.1.108 -p 135
+			cuckoo -t 192.168.1.108 -p 5555
+		''')
+	)
+
+	parser.add_argument('-c', '--command', action = 'store_true', help = 'command shell')
+	parser.add_argument('-e', '--execute', help = 'execute specified command')
+	parser.add_argument('-l', '--listen', action = 'store_true', help = 'listen')
+	parser.add_argument('-p', '--port', type = int, default = 5555, help = 'specified port')
+	parser.add_argument('-t', '--target', default = '192.168.1.203', help = 'specified IP')
+	parser.add_argument('-u', '--upload', help = 'upload file')
+
+	args = parser.parse_args()
+
+	if args.listen:
+		buffer = ''
+	else:
+		buffer = sys.stdin.read()
+
+	nc = NetCat(args, buffer.encode())
+	nc.run()
+
+if __name__ == '__main__':
+    main()
